59c67d4a69be94097736e91eefb5ff9790d7dad9
uram <parkuram12@gmail.com>
2022-07-05 23:50:02 +0900

    [feature] 15th commit - 클래스 변수명 변경

--- src/main/java/study/baseball/BaseBallGameOutput.java
+++ src/main/java/study/baseball/BaseBallGameOutput.java
@@ -2,15 +2,15 @@ package study.baseball;
 
 public class BaseBallGameOutput {
 
-    private static final String startMessage = "숫자를 입력해 주세요 : ";
-    private static final String retryMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+    private static final String START_MESSAGE = "숫자를 입력해 주세요 : ";
+    private static final String RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
 
     public void startPrint() {
-        System.out.println(startMessage);
+        System.out.println(START_MESSAGE);
     }
 
     public void retryPrint() {
-        System.out.println(retryMessage);
+        System.out.println(RETRY_MESSAGE);
     }
 
     public void scorePrint(Score score) {
